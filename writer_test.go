0695d72049d55353681c21ed51d9610585ee8198
Marcin Gorzynski <marcin@gorzynski.me>
2024-06-06 16:26:39 +0200

    ut: more ut and write bench - remove comment

--- writer_test.go
+++ writer_test.go
@@ -11,8 +11,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TODO: write generic tests with diffrent encoders / decoders and compressors
-
 func TestWriter_Write(t *testing.T) {
 	blocksFile := Blocks[int]{
 		{

dbf757bda7b3c1369d28b690e26e5786e04cc6ed
Marcin Gorzynski <marcin@gorzynski.me>
2025-09-26 10:16:55 +0200

    Add Parent hash to Block and provide Writer that verifies correctness

---
+++ writer_with_verify_hash.go
@@ -0,0 +1,70 @@
+package ethwal
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/0xsequence/ethkit/go-ethereum/common"
+)
+
+type BlockHashGetter func(ctx context.Context, blockNum uint64) (common.Hash, error)
+
+func BlockGetterFromReader[T any](options Options) BlockHashGetter {
+	return func(ctx context.Context, blockNum uint64) (common.Hash, error) {
+		reader, err := NewReader[T](options)
+		if err != nil {
+			return common.Hash{}, fmt.Errorf("failed to create reader: %w", err)
+		}
+		defer reader.Close()
+
+		err = reader.Seek(ctx, blockNum-1)
+		if err != nil {
+			return common.Hash{}, fmt.Errorf("failed to seek to block %d: %w", blockNum, err)
+		}
+
+		block, err := reader.Read(ctx)
+		if err != nil {
+			return common.Hash{}, fmt.Errorf("failed to read block %d: %w", blockNum, err)
+		}
+		return block.Hash, nil
+	}
+}
+
+type writerWithVerifyHash[T any] struct {
+	Writer[T]
+
+	blockHashGetter BlockHashGetter
+
+	prevHash common.Hash
+}
+
+var _ Writer[any] = (*writerWithVerifyHash[any])(nil)
+
+func NewWriterWithVerifyHash[T any](writer Writer[T], blockHashGetter BlockHashGetter) Writer[T] {
+	return &writerWithVerifyHash[T]{Writer: writer, blockHashGetter: blockHashGetter}
+}
+
+func (w *writerWithVerifyHash[T]) Write(ctx context.Context, b Block[T]) error {
+	var err error
+	if w.prevHash == (common.Hash{}) && b.Number > 1 {
+		w.prevHash, err = w.blockHashGetter(ctx, b.Number-1)
+		if err != nil {
+			return err
+		}
+	}
+
+	if b.Parent != w.prevHash {
+		w.prevHash = common.Hash{}
+		return fmt.Errorf("parent hash mismatch, expected %s, got %s",
+			w.prevHash.String(), b.Parent.String())
+	}
+
+	err = w.Writer.Write(ctx, b)
+	if err != nil {
+		w.prevHash = common.Hash{}
+		return err
+	}
+
+	w.prevHash = b.Hash
+	return nil
+}

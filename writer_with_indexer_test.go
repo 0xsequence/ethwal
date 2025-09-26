49bfa485792baa09f8659a4b3c10a64cdf99e82e
Marcin Gorzynski <marcin@gorzynski.me>
2025-09-26 10:24:19 +0200

    restore tests

---
+++ writer_with_indexer_test.go
@@ -0,0 +1,59 @@
+package ethwal
+
+import (
+	"context"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWriterWithIndexer(t *testing.T) {
+	defer func() {
+		_ = os.RemoveAll(testPath)
+	}()
+
+	blocks := generateMixedIntBlocks()
+
+	indexes := generateMixedIntIndexes()
+
+	indexer, err := NewIndexer(context.Background(), IndexerOptions[[]int]{
+		Dataset: Dataset{
+			Path: testPath,
+		},
+		Indexes: indexes,
+	})
+	require.NoError(t, err)
+
+	w, err := NewWriter[[]int](Options{
+		Dataset: Dataset{
+			Path: testPath,
+		},
+		NewCompressor: NewZSTDCompressor,
+		NewEncoder:    NewCBOREncoder,
+	})
+	require.NoError(t, err)
+
+	wi, err := NewWriterWithIndexer(w, indexer)
+	require.NoError(t, err)
+
+	for _, block := range blocks {
+		err := wi.Write(context.Background(), block)
+		require.NoError(t, err)
+	}
+
+	err = wi.RollFile(context.Background())
+	require.NoError(t, err)
+
+	err = wi.Close(context.Background())
+	require.NoError(t, err)
+
+	indexDirEntries, err := os.ReadDir(path.Join(testPath, ".indexes"))
+	require.NoError(t, err)
+	require.Len(t, indexDirEntries, 4)
+
+	ethwalDirEntries, err := os.ReadDir(testPath)
+	require.NoError(t, err)
+	require.Len(t, ethwalDirEntries, 3)
+}
